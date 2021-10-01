05110195797476624ab5c6b7567b848c5e3b7742
SumedhBengale <ssbengale@gmail.com>
2021-10-01 22:34:28 +0530

    Basic React Test Site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Checkbox from '@mui/material/Checkbox';
+import SaveIcon from '@mui/icons-material/AddAlert'
+import DeleteIcon from '@mui/icons-material/AddAlert';
+import { FormControlLabel } from '@mui/material';
+import { TextField } from '@mui/material';
+import { Container } from '@mui/material';
+import { Paper } from '@mui/material';
+import { Grid } from '@mui/material';
+
+function CheckboxExample(){
+  const{checked}=React.useState(true)
+  return (
+    <FormControlLabel
+      control={
+      <Checkbox
+        icon={<SaveIcon></SaveIcon>}
+        checkedIcon={<SaveIcon></SaveIcon>}
+        defaultChecked={checked}
+        color='primary'
+        inputProps={{
+          'aria-label':'secondary checkbox',
+        }}>
+      </Checkbox>}
+      label='Testing Checkbox'>
+    </FormControlLabel>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='lg'>
+      <div className='App'>
+        <header className='App-header'>
+          <TextField
+          variant='outlined'
+          type='email'
+          label='Email'
+          // placeholder='test@yourmail.com'
+          ></TextField>
+          <CheckboxExample></CheckboxExample>
+          <ButtonGroup variant='outlined' color='primary'>
+            <Button startIcon={<SaveIcon/>} onClick={()=>alert("Saved")} >Save</Button>
+            <Button startIcon={<DeleteIcon/>} onClick={()=>alert("Delete")} >Delete</Button>
+          </ButtonGroup>
+          <img src={logo} classname='App-logo' alt='logo'></img>
+        </header>
+      </div>
+    </Container>
   );
 }
 
